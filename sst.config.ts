856133a0bea11ba10003779d59283dd800e2296b
kksteini <steini@kosmosogkaos.is>
2025-05-06 18:42:37 +0000

    Change sst namespace

--- sst.config.ts
+++ sst.config.ts
@@ -3,7 +3,7 @@
 export default $config({
   app(input) {
     return {
-      name: 'island-bid-starter-backend',
+      name: 'island-bid-registry',
       removal: input?.stage === 'production' ? 'retain' : 'remove',
       protect: ['production'].includes(input?.stage),
       home: 'aws',
