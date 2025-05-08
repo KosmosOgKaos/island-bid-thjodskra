afbe1643a5a867f3d4ed43a77f44487118bd9630
krillijuni <kristleifur@juni.is>
2025-05-08 09:09:50 +0000

    Add ME to the DB

--- insert_data.sql
+++ insert_data.sql
@@ -1,10 +1,20 @@
 INSERT INTO "Person" (name, kennitala, address, email, telephone, "createdAt", "updatedAt")
-VALUES (
-    'Jökull Þórðarson',
-    '1203894569',
-    'Bláfjallagata 12, 105 Reykjavík',
-    'jokull.thordarson@email.is',
-    '772-8391',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-)
+VALUES 
+    (
+        'Jökull Þórðarson',
+        '1203894569',
+        'Bláfjallagata 12, 105 Reykjavík',
+        'jokull.thordarson@email.is',
+        '772-8391',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    ),
+    (
+        'Kristleifur Þorsteinsson',
+        '2811882089',
+        'Háaleitisbraut 17, 108 Reykjavík',
+        'kristleifur.thorsteinsson@email.is',
+        '5556969',
+        CURRENT_TIMESTAMP,
+        CURRENT_TIMESTAMP
+    );
